cb10e32b93707d6546c13c43aad0145bc9dfc1ec
Pedro Lopes <pedrolopes.info@gmail.com>
2019-10-26 17:15:13 -0300

    like feature

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = new express.Router();
 const upload = multer(uploadConfig);
 
 routes.get('/posts', PostController.index);
 routes.post('/posts', upload.single('image'), PostController.store);
+routes.post('/posts/:id/like', LikeController.store);
 
 module.exports = routes;
