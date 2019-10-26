8e61115c672c20aec56f634ed44fac0697203179
Pedro Lopes <pedrolopes.info@gmail.com>
2019-10-26 17:34:48 -0300

    resize images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:mandrake@cluster0-aci6o.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
